d2c83a5c2e20c61eb7bccda87c68302efa3d70de
tahupetis30 <80400375+tahupetis30@users.noreply.github.com>
2021-03-11 00:08:56 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -2,28 +2,28 @@
 
 error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 echo "
- _____ ____________
-|                  |
-| Tahu Petis tools |
-___________________
- [Kediri Black Hat]  Directory Brute\n\n";
+ _____ _______________
+| Open Journal System|
+|  Directory Brute   |
+______________________
+ [Kediri Black Hat]  \n\n";
 $pat = array("/files/","/file/","/journals/","/journal/","/jurnal/","/jurnals/",
 "/jurnal_file","/jurnal_files/","/jurnal_data_file/","/jurnal_data_files/",
 "/jurnalfile/","/jurnal_dat0_file/","/jurnalfiles/","/jurnaldatafile/","/jurnaldatafiles/","/data/",
 "/datafile/","/datafiles/","/journalfile/","/journalfiles/","/journal_file/","/journal_files/",
 "/journal_data/","/journal_data_file/","/journal_data_files/");
-echo "Apakah aku sangat sayang nchi?: ";
+echo "Masukan websitemu: ";
 $wb = trim(fgets(STDIN));
 foreach($pat as $path) {
 $c3k = get_headers($wb.$path);
 if (preg_match("/200/", $c3k[0])) {
-echo "[Gamteng] Zeeb > $wb/$path \nSudah Selesai Tuan \n";
+echo "[Berhasil] Zeeb > $wb/$path \nSudah Selesai Tuan \n";
 exit;
 } elseif (preg_match("/403/", $c3k[0])) {
 echo "[403] On > $wb/$path \nSudah Selesai Tuan \n";
 exit;
 } else {
-echo "[Burik] Sangat mencintai nchi:) > $wb/$path \n";
+echo "[Gagal] Menacri directory > $wb/$path \n";
 }
 }
 echo "Sudah Selesai Tuan\n";
